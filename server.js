6476deb78f219f8d41cf5934cf93352c9e9135ba
Renzo <19870186+Bigg-Iron@users.noreply.github.com>
2017-11-07 10:16:39 -0800

    Added handlebars code to server.js and modified burger controller

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-  })
-  
-  app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-  })
+var bodyParser = require('body-parser');
+
+var port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var exphbs = require('express-handlebars');
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+var routes = require("./burger/controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+app.listen(port);
+
+
+console.log('App listening on port: ' + port );
